eedbff45e7fc64e3bfae315cd4f8d468c1ac3661
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-09-10 13:20:28 -0700

    add edit capability to ticket control and add edit ticket form component

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,103 +1,117 @@
-import React from "react";
-import NewTicketForm from "./NewTicketForm";
-import TicketList from "./TicketList";
-import TicketDetail from "./TicketDetail";
-import EditTicketForm from "./EditTicketForm";
+import React from 'react';
+import NewTicketForm from './NewTicketForm';
+import TicketList from './TicketList';
+import TicketDetail from './TicketDetail';
+import EditTicketForm from './EditTicketForm';
 
 class TicketControl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      formVisibleOnPage: false,
-      masterTicketList: [],
-      selectedTicket: null,
-      editing: false,
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			formVisibleOnPage: false,
+			masterTicketList: [],
+			selectedTicket: null,
+			editing: false,
+		};
+	}
 
-  handleClick = () => {
-    if (this.state.selectedTicket != null) {
-      this.setState({
-        formVisibleOnPage: false,
-        selectedTicket: null,
-      });
-    } else {
-      this.setState((prevState) => ({
-        formVisibleOnPage: !prevState.formVisibleOnPage,
-      }));
-    }
-  };
+	handleClick = () => {
+		if (this.state.selectedTicket != null) {
+			this.setState({
+				formVisibleOnPage: false,
+				selectedTicket: null,
+				editing: false,
+			});
+		} else {
+			this.setState((prevState) => ({
+				formVisibleOnPage: !prevState.formVisibleOnPage,
+			}));
+		}
+	};
 
-  handleEditClick = () => {
-    console.log("handleEditClick reached!");
-    this.setState({ editing: true });
-  };
+	handleEditClick = () => {
+		console.log('handleEditClick reached!');
+		this.setState({ editing: true });
+	};
 
-  handleChangingSelectedTicket = (id) => {
-    const selectedTicket = this.state.masterTicketList.filter(
-      (ticket) => ticket.id === id
-    )[0];
-    this.setState({ selectedTicket: selectedTicket });
-  };
+	handleEditingTicketInList = (ticketToEdit) => {
+		const editedMasterTicketList = this.state.masterTicketList
+			.filter((ticket) => ticket.id !== this.state.selectedTicket.id)
+			.concat(ticketToEdit);
+		this.setState({
+			masterTicketList: editedMasterTicketList,
+			editing: false,
+			selectedTicket: null,
+		});
+	};
+	handleChangingSelectedTicket = (id) => {
+		const selectedTicket = this.state.masterTicketList.filter(
+			(ticket) => ticket.id === id
+		)[0];
+		this.setState({ selectedTicket: selectedTicket });
+	};
 
-  handleAddingNewTicketToList = (newTicket) => {
-    const newMasterTicketList = this.state.masterTicketList.concat(newTicket);
-    this.setState({
-      masterTicketList: newMasterTicketList,
-      formVisibleOnPage: false,
-    });
-  };
+	handleAddingNewTicketToList = (newTicket) => {
+		const newMasterTicketList = this.state.masterTicketList.concat(newTicket);
+		this.setState({
+			masterTicketList: newMasterTicketList,
+			formVisibleOnPage: false,
+		});
+	};
 
-  handleDeletingTicket = (id) => {
-    const newMasterTicketList = this.state.masterTicketList.filter(
-      (ticket) => ticket.id !== id
-    );
-    this.setState({
-      masterTicketList: newMasterTicketList,
-      selectedTicket: null,
-    });
-  };
+	handleDeletingTicket = (id) => {
+		const newMasterTicketList = this.state.masterTicketList.filter(
+			(ticket) => ticket.id !== id
+		);
+		this.setState({
+			masterTicketList: newMasterTicketList,
+			selectedTicket: null,
+		});
+	};
 
-  render() {
-    let currentlyVisibleState = null;
-    let buttonText = null;
+	render() {
+		let currentlyVisibleState = null;
+		let buttonText = null;
 
-    if (this.state.editing) {
-      currentlyVisibleState = (
-        <EditTicketForm ticket={this.state.selectedTicket} />
-      );
-      buttonText = "Return to Ticket List";
-    } else if (this.state.selectedTicket != null) {
-      currentlyVisibleState = (
-        <TicketDetail
-          ticket={this.state.selectedTicket}
-          onClickingDelete={this.handleDeletingTicket}
-          onClickingEdit={this.handleEditClick}
-        />
-      );
-      buttonText = "Return to Ticket List";
-    } else if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = (
-        <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
-      );
-      buttonText = "Return to Ticket List";
-    } else {
-      currentlyVisibleState = (
-        <TicketList
-          ticketList={this.state.masterTicketList}
-          onTicketSelection={this.handleChangingSelectedTicket}
-        />
-      );
-      buttonText = "Add Ticket";
-    }
+		if (this.state.editing) {
+			currentlyVisibleState = (
+				<EditTicketForm
+					ticket={this.state.selectedTicket}
+					onEditTicket={this.handleEditingTicketInList}
+				/>
+			);
+			buttonText = 'Return to Ticket List';
+		} else if (this.state.selectedTicket != null) {
+			currentlyVisibleState = (
+				<TicketDetail
+					ticket={this.state.selectedTicket}
+					onClickingDelete={this.handleDeletingTicket}
+					onClickingEdit={this.handleEditClick}
+				/>
+			);
+			buttonText = 'Return to Ticket List';
+		} else if (this.state.formVisibleOnPage) {
+			currentlyVisibleState = (
+				<NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
+			);
+			buttonText = 'Return to Ticket List';
+		} else {
+			currentlyVisibleState = (
+				<TicketList
+					ticketList={this.state.masterTicketList}
+					onTicketSelection={this.handleChangingSelectedTicket}
+				/>
+			);
+			buttonText = 'Add Ticket';
+		}
 
-    return (
-      <React.Fragment>
-        {currentlyVisibleState}
-        <button onClick={this.handleClick}>{buttonText}</button>
-      </React.Fragment>
-    );
-  }
+		return (
+			<React.Fragment>
+				{currentlyVisibleState}
+				<button onClick={this.handleClick}>{buttonText}</button>
+			</React.Fragment>
+		);
+	}
 }
 
 export default TicketControl;
